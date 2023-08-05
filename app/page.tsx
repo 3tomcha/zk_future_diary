87b793483d8f255a88982ddbe3e20f78774d4d78
Tomoya Kobayashi <tomocha.marika@gmail.com>
2023-08-05 12:51:46 +0900

    change to layout dir

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
-import { useAccount, useConnect } from 'wagmi'
+import { useAccount, useConnect, usePrepareContractWrite } from 'wagmi'
 import { InjectedConnector } from 'wagmi/connectors/injected'
+// import ContractAbi from '../abi/Contract.abi'
 
 export default function Home() {
   const { address, isConnected } = useAccount();
@@ -8,6 +9,10 @@ export default function Home() {
     connector: new InjectedConnector
   });
 
+  const { config } = usePrepareContractWrite({
+    address: "0x05C4AE6bC33e6308004a47EbFa99E5Abb4133f86", //goerli testnet
+    // abi: ContractAbi,
+  })
   if (isConnected) {
     return (<div>connected to {address}</div>)
   }
