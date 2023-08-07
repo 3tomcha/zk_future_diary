cf0d3c7a073063544d4602fc60a5a4db20ca22d6
Tomoya Kobayashi <tomocha.marika@gmail.com>
2023-08-07 20:12:16 +0900

    succeeded to get proof

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 'use client';
 import { useAccount, useConnect } from 'wagmi'
 import { InjectedConnector } from 'wagmi/connectors/injected'
-import { IDKitWidget, CredentialType } from "@worldcoin/idkit"
+import { IDKitWidget, CredentialType, ISuccessResult } from "@worldcoin/idkit"
+import { useEffect, useState } from 'react';
 
 export default function Home() {
   const { address, isConnected } = useAccount();
+  const [proof, setProof] = useState<ISuccessResult | null>(null);
 
   const { connect } = useConnect({
     connector: new InjectedConnector
@@ -14,20 +16,19 @@ export default function Home() {
     return (<div>connected to {address}</div>)
   }
 
-  const onSuccess = () => {
-    console.log('success')
-  }
 
   return (
-    <IDKitWidget
-      app_id='app_staging_abe19aaaafb96a06a7c45fc82138fe88'
-      action="test"
-      signal={address}
-      onSuccess={onSuccess}
-      credential_types={[CredentialType.Orb]}
-      enableTelemetry
-    >
-      {({ open }) => <button onClick={open}>Verify with World ID</button>}
-    </IDKitWidget>
+    <> {proof && <div>{JSON.stringify(proof)}</div>}
+      <IDKitWidget
+        app_id='app_staging_abe19aaaafb96a06a7c45fc82138fe88'
+        action="test"
+        signal={address}
+        onSuccess={setProof}
+        credential_types={[CredentialType.Orb]}
+        enableTelemetry
+      >
+        {({ open }) => <button onClick={open}>Verify with World ID</button>}
+      </IDKitWidget>
+    </>
   )
 }
