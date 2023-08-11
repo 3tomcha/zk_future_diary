ea6c23a3175576622afcaf5aefa6a37c802ce49a
Tomoya Kobayashi <tomocha.marika@gmail.com>
2023-08-11 16:48:54 +0900

    suuceeded to display map

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,1 +1,17 @@
-import { MapContainer, TileLayer, useMap } from 'react-leaflet'
+"use client"
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
+import "./map.css"
+import 'leaflet/dist/leaflet.css';
+
+export default function Map() {
+  return (
+    <div className="map">
+      <MapContainer center={[35.6895, 139.6917]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://basemaps.cartocdn.com/copyright">Basemaps</a> contributors'
+          url="https://{s}.basemaps.cartocdn.com/rastertiles/dark_all/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+    </div>
+  )
+}
