9b1ba53097f7bf568eb6e50c8dea9e8b72678574
Tomoya Kobayashi <tomocha.marika@gmail.com>
2024-04-29 16:49:09 +0900

    fix post issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,5 @@
 'use client';
-import { configureChains, createConfig, WagmiConfig } from "wagmi";
-import { optimismGoerli } from "wagmi/chains";
-import { publicProvider } from 'wagmi/providers/public';
 import RecoilProvider from './recoilProvider'
-
-const { publicClient, webSocketPublicClient } = configureChains(
-  [optimismGoerli],
-  [publicProvider()]
-)
-
-const config = createConfig({
-  publicClient,
-  webSocketPublicClient
-})
 
 export default function RootLayout({
   children,
@@ -20,13 +7,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <WagmiConfig config={config}>
-      <html lang="ja">
-        <head />
-        <body>
-          <RecoilProvider>{children}</RecoilProvider>
-        </body>
-      </html>
-    </WagmiConfig>
+    <html lang="ja">
+      <head />
+      <body>
+        <RecoilProvider>{children}</RecoilProvider>
+      </body>
+    </html>
   );
 }
