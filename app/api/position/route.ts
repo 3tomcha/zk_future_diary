adfbd762848bdd4c119e2e15e17c572bc2b4209c
Tomoya Kobayashi <tomocha.marika@gmail.com>
2024-04-29 09:31:33 +0900

    rough sign

--- app/api/position/route.ts
+++ app/api/position/route.ts
@@ -1,55 +1,46 @@
-import { NextRequest } from 'next/server'
+import { NextRequest, NextResponse } from 'next/server'
 import fetch from 'node-fetch'
 // import pinataSDK from "@pinata/sdk";
+// @ts-ignore
+import Client from 'mina-signer';
+import { GoogleMapsResponse, PlaceResult } from '../../types/googlemap';
+const client = new Client({ network: 'testnet' });
 
-type GoogleMapsResponse = {
-  status: string;
-  results: PlaceResult[];
-  error_message?: string;
+// Implement toJSON for BigInt so we can include values in response
+(BigInt.prototype as any).toJSON = function () {
+  return this.toString();
 };
 
-type PlaceResult = {
-  formatted_address: string;
-  geometry: {
-    location: {
-      lat: number;
-      lng: number;
-    };
-    viewport?: {
-      northeast: { lat: number; lng: number };
-      southwest: { lat: number; lng: number };
-    };
-  };
-  name: string;
-  opening_hours?: {
-    open_now: boolean;
-  };
-  photos?: {
-    height: number;
-    width: number;
-    photo_reference: string;
-  }[];
-  place_id: string;
-  rating?: number;
-  reference: string;
-  types: string[];
-};
+function getSignedGeometry(locations: {
+  lat: number;
+  lng: number;
+}[]) {
+  let privateKey =
+    process.env.PRIVATE_KEY ??
+    'EKF65JKw9Q1XWLDZyZNGysBbYG21QbJf3a4xnEoZPZ28LKYGMw53';
 
+  const signature = client.signFields(
+    [locations],
+    privateKey
+  )
+}
 
-export async function GET(req: NextRequest) {
+async function getGoogleMapResponse() {
   const GOOGLEMAP_API_KEY = process.env.GOOGLEMAP_API_KEY
   console.log(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+Tokyo&key=${GOOGLEMAP_API_KEY}`)
   const response = await fetch(
     `https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+Tokyo&key=${GOOGLEMAP_API_KEY}`
   )
   const responseJSON = await response.json() as GoogleMapsResponse
-  const geometries = responseJSON.results.map(r => r.geometry)
-  console.log(geometries)
+  const locations = responseJSON.results.map(r => r.geometry.location)
+  console.log(locations)
+  return locations
+}
 
-  return new Response(JSON.stringify(geometries), {
-    status: 200,
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  });
+export async function GET(req: NextRequest) {
+  const locations = await getGoogleMapResponse()
+  return NextResponse.json(
+    getSignedGeometry(locations),
+    { status: 200 }
+  );
 }
