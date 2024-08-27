9f5a60467bb318382e5f970fbe126db7a56dddef
Tomoya Kobayashi <tomocha.marika@gmail.com>
2024-08-27 18:45:37 +0900

    apply schedule item

--- ui/src/components/ScheduleItem.tsx
+++ ui/src/components/ScheduleItem.tsx
@@ -1,25 +1,26 @@
-import "./ScheduleItem.css"
+import styles from "./ScheduleItem.module.css";
 
 type Schedule = {
   time: string;
   value: string;
   image?: string;
   onVerify: () => void;
-}
+};
+
 export default function ScheduleItem({ time, value, image, onVerify }: Schedule) {
   return (
     <>
-      <li className="schedule-item">
+      <li className={styles.scheduleItem}>
         <div>
-          <span className="time">{time}</span>: {value}
+          <span className={styles.time}>{time}</span>: {value}
         </div>
-        <div className="verify-buttons">
-          <button className="verify" onClick={onVerify}>verify time</button>
-          <button className="verify" onClick={onVerify}>verify location</button>
-          <button className="verify" onClick={onVerify}>verify activity</button>
+        <div className={styles.verifyButtons}>
+          <button className={styles.verify} onClick={onVerify}>verify time</button>
+          <button className={styles.verify} onClick={onVerify}>verify location</button>
+          <button className={styles.verify} onClick={onVerify}>verify activity</button>
         </div>
       </li>
       {image && <img src={image} alt="aa" />}
     </>
-  )
+  );
 }
--- ui/src/pages/index.tsx
+++ ui/src/pages/index.tsx
@@ -1,6 +1,34 @@
 import { useEffect, useState } from "react";
 import ZkappWorkerClient from "../zkappWorkerClient";
 import { Field, PublicKey, Signature } from "o1js";
+import ScheduleItem from "@/components/ScheduleItem";
+
+const mockSchedule = [
+  { "time": "00:00", "value": "睡眠" },
+  { "time": "01:00", "value": "睡眠" },
+  { "time": "02:00", "value": "睡眠" },
+  { "time": "03:00", "value": "睡眠" },
+  { "time": "04:00", "value": "睡眠" },
+  { "time": "05:00", "value": "ランニング" },
+  { "time": "06:00", "value": "シャワー" },
+  { "time": "07:00", "value": "朝食: 野菜スムージー" },
+  { "time": "08:00", "value": "仕事" },
+  { "time": "09:00", "value": "仕事" },
+  { "time": "10:00", "value": "スナック: キウイ" },
+  { "time": "11:00", "value": "仕事" },
+  { "time": "12:00", "value": "ランチ: グリルチキンとサラダ" },
+  { "time": "13:00", "value": "仕事" },
+  { "time": "14:00", "value": "仕事" },
+  { "time": "15:00", "value": "スナック: プロテインバー" },
+  { "time": "16:00", "value": "ジム: カーディオトレーニング" },
+  { "time": "17:00", "value": "ジム: ウェイトトレーニング" },
+  { "time": "18:00", "value": "夕食: サーモンと野菜" },
+  { "time": "19:00", "value": "ストレッチとリラックス" },
+  { "time": "20:00", "value": "読書" },
+  { "time": "21:00", "value": "軽いヨガ" },
+  { "time": "22:00", "value": "夜のおやつ: ギリシャヨーグルト" },
+  { "time": "23:00", "value": "睡眠" },
+]
 
 type StateType = {
   zkappWorkerClient: ZkappWorkerClient | null;
@@ -67,7 +95,7 @@ export default function Home() {
     })()
   }, [])
 
-  const handleUpdate = async () => {
+  const handleVerify = async () => {
     const response = await fetch("/api/time")
     const data = await response.json()
     console.log(data)
@@ -98,7 +126,17 @@ export default function Home() {
     console.log(hash)
   }
 
-  return (
-    <button onClick={handleUpdate}>handleUpdate</button>
-  )
+  if (state.hasWallet) {
+    return (
+      <div className="container">
+        <ul className="schedule-list" id="schedule">
+          {mockSchedule.map((item) => {
+            return (
+              <ScheduleItem {...item} key={item.time} onVerify={handleVerify} />
+            )
+          })}
+        </ul>
+      </div>
+    )
+  }
 }
